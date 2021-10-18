310f63ab4cfe80c44c9a0ada53663010d085a7b6
darshilagrawal <darshilagrawal2000@gmail.com>
2021-10-18 19:11:53 +0530

    Main diffable Tableviewcontroller

---
+++ Diffably/DataViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DataViewController.swift
+//  Diffably
+//
+//  Created by Darshil Agrawal on 18/10/21.
+//
+
+import Foundation
+import UIKit
+
+class DataViewController: UITableViewController {
+    
+    var dataEntries = [DataToDisplay]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
