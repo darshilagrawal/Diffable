369ae61300460ced77449291fdbea1df54c8e20c
darshilagrawal <darshilagrawal2000@gmail.com>
2021-10-19 22:13:12 +0530

    Added dequeue method

--- Diffably/DataViewController.swift
+++ Diffably/DataViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class DataViewController: UITableViewController {
     
+    enum Section {
+        case main
+    }
     var dataEntries = [DataToDisplay]()
+    var dataSource: UITableViewDiffableDataSource<Section,DataToDisplay>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section,DataToDisplay>(tableView: tableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = itemIdentifier.name
+            return cell
+        })
     }
     
 }
