1be5c9b53d1dbd4cb4c32db182b832a195d40804
darshilagrawal <darshilagrawal2000@gmail.com>
2021-10-21 21:29:02 +0530

    Data Addition methods

--- Diffably/ViewController.swift
+++ Diffably/ViewController.swift
@@ -10,19 +10,41 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableview: UITableView!
+    var dataSource: UITableViewDiffableDataSource<Section,DataToDisplay>!
+    enum Section {
+        case main
+    }
+    var dataEntries = [DataToDisplay]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addData))
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.right"), style: .plain, target: self, action: #selector(navigateNext))
+        configureDataSource()
     }
     
     @objc func addData() {
-        
+        let alert = UIAlertController(title: "Create User", message: nil, preferredStyle: .alert)
+        alert.addTextField{$0.placeholder = "Enter the Data"}
+        let action = UIAlertAction(title: "Save", style: .default) { _ in
+            let userName = alert.textFields?.first?.text ?? ""
+            let newData = DataToDisplay(name: userName)
+            self.dataEntries.append(newData)
+        }
+        alert.addAction(action)
     }
     
     @objc func navigateNext() {
         let vc = DataViewController()
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section,DataToDisplay>(tableView: tableview, cellProvider: { tableView, indexPath, itemIdentifier in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = itemIdentifier.name
+            return cell
+        })
     }
 
 
